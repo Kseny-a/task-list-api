96419e315aa30684175aaaea111790536d555298
Aksana <adziamidava@gmail.com>
2024-11-01 22:12:12 -0700

    implemented task model, generated migration

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,15 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from ..db import db
+from datetime import datetime
+
+#from sqlalchemy import DateTime
+from typing import Optional
+
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+    description: Mapped[str]
+    completed_at: Mapped[Optional[datetime]]
+
+    #completed_at: Mapped[Optional[datetime]] = mapped_column(default=None)
--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,1 +1,47 @@
-from flask import Blueprint
+from flask import Blueprint, abort, make_response
+from app.models.task import Task
+
+tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
+
+# @tasks_bp.get("")
+# def get_all_tasks():
+#     tasks_response = []
+#     for task in tasks:
+#         tasks_response.append(
+#             {
+#                 "id": task.id,
+#                 "title": task.title,
+#                 "description": task.description,
+#                 "completed_at": task.completed_at 
+#             }
+#         )
+#     return tasks_response
+
+# @tasks_bp.get("/<task_id>")
+# def get_one_task(task_id):
+#     task = validate_task_id(task_id)
+
+#     return {
+#                 "id": task.id,
+#                 "title": task.title,
+#                 "description": task.description,
+#                 "completed_at": task.completed_at 
+#             }
+
+# def validate_task_id()
+#     try:
+#         task_id = int(task_id)
+#     except:
+#         response =  {"message": "Task {task_id} invalid."}
+#         abort(make_response(response, 400))  
+
+#     for task in tasks:
+#         if task.id == task_id:
+#             return task
+        
+#     response =  {"message": "Task {task_id} not found."}
+#     abort(make_response(response, 404))      
+
+
+
+
