a61305570546c2446f09618ec9bf88019a7238f1
Aksana <adziamidava@gmail.com>
2024-11-02 00:47:45 -0700

    implemented routes to get all tasks, one task, create a task, delete a task and update a task

--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,47 +1,93 @@
-from flask import Blueprint, abort, make_response
+from flask import Blueprint, abort, make_response, request, Response
 from app.models.task import Task
+from ..db import db
 
 tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
 
-# @tasks_bp.get("")
-# def get_all_tasks():
-#     tasks_response = []
-#     for task in tasks:
-#         tasks_response.append(
-#             {
-#                 "id": task.id,
-#                 "title": task.title,
-#                 "description": task.description,
-#                 "completed_at": task.completed_at 
-#             }
-#         )
-#     return tasks_response
+@tasks_bp.get("")
+def get_all_tasks():
+    query = db.select(Task).order_by(Task.id)
+    tasks = db.session.scalars(query)
+    tasks_response = []
+    for task in tasks:
+        tasks_response.append(
+            {
+                "id": task.id,
+                "title": task.title,
+                "description": task.description,
+                "is_completed": bool(task.completed_at) 
+            }
+        )
+    return tasks_response
 
-# @tasks_bp.get("/<task_id>")
-# def get_one_task(task_id):
-#     task = validate_task_id(task_id)
+@tasks_bp.get("/<task_id>")
+def get_one_task(task_id):
+    task = validate_task_id(task_id)
 
-#     return {
-#                 "id": task.id,
-#                 "title": task.title,
-#                 "description": task.description,
-#                 "completed_at": task.completed_at 
-#             }
+    return {
+                "id": task.id,
+                "title": task.title,
+                "description": task.description,
+                "is_completed": bool(task.completed_at)
+            }
 
-# def validate_task_id()
-#     try:
-#         task_id = int(task_id)
-#     except:
-#         response =  {"message": "Task {task_id} invalid."}
-#         abort(make_response(response, 400))  
+def validate_task_id(task_id):
+    try:
+        task_id = int(task_id)
+    except:
+        response =  {"message": f"Task {task_id} invalid."}
+        abort(make_response(response, 400))
 
-#     for task in tasks:
-#         if task.id == task_id:
-#             return task
-        
-#     response =  {"message": "Task {task_id} not found."}
-#     abort(make_response(response, 404))      
+    query = db.select(Task).where(Task.id==task_id)
+    task = db.session.scalar(query)
+    if not task:
+        response =  {"message": f"Task {task_id} not found."}
+        abort(make_response(response, 404))   
 
+    return task       
 
+@tasks_bp.post("")
+def create_task():
+    request_body = request.get_json()
 
+    title = request_body["title"]
+    description = request_body["description"]
+    completed_at = request_body["completed_at"]
+    #if not completed_at:
+    #    completed_at = None
 
+    new_task = Task(title=title, description=description, completed_at=completed_at)
+    db.session.add(new_task)
+    db.session.commit()
+
+    response = {
+                "id": new_task.id,
+                "title": new_task.title,
+                "description": new_task.description,
+                "is_completed": bool(new_task.completed_at) 
+            }
+    return response, 201
+
+@tasks_bp.put("/<task_id>")
+def update_task(task_id):
+    task = validate_task_id(task_id)
+    request_body = request.get_json()
+
+    task.title = request_body["title"]
+    task.description = request_body["description"]
+    #task.completed_at = request_body["completed_at"]
+
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
+
+@tasks_bp.delete("/<task_id>")
+def delete_task(task_id):
+    task = validate_task_id(task_id)
+    db.session.delete(task)
+    db.session.commit()
+
+    response = {
+                "details": f"Task {task_id} successfully deleted."
+            }
+    return response, 200
